4fc207187e62776c333a9c9828c31d365e2aea9d
rodier <clinrodier@yahoo.fr>
2026-01-20 00:50:07 +0100

    Use the Provider

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.6.1"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "create_directory" {
+  filename = "${path.module}/my-directory/.gitkeep"
+  content = ""
+}
+
+resource "local_file" "create_file" {
+  filename = "${path.module}/my-directory/patou.txt"
+  content  = "This is a sample file created by Terraform."
+}
